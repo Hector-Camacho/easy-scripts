0020aeaaaf5992cccd5054d794f8ed75b2b348c6
Hector-Camacho <hector.camacho.f@gmail.com>
2017-05-10 11:28:39 -0500

    [ADD] Added developer tools to monster_reborn

--- monster_reborn.sh
+++ monster_reborn.sh
@@ -1,38 +1,48 @@
 #!/bin/bash
+VERDE='\033[0;32m'
+NC='\033[0m' # No Color
 
 # ------------------------- Actualizando sistema ----------------------- 
-echo "[+] Actualizando sistema..."
+echo "${VERDE}[+]${NC} Actualizando sistema..."
 sudo apt-get update
 sudo apt-get -y upgrade
 
 # ------------------------- Instalando ambiente para odoo -------------- 
-echo "[+] Instalando pip ipdb odoorpc"
+echo "${VERDE}[+]${NC} Instalando pip ipdb odoorpc"
 sudo apt-get -y install python-pip
 sudo pip install odoorpc
 sudo pip install ipdb
 
-echo "[+] Instalando git"
+echo "${VERDE}[+]${NC} Instalando git"
 sudo apt-get -y install git git-core
 
 # -------------------------- Instalando odoo --------------------------- 
-echo "[+] Clonando odoo comunity en dentro de la carpeta dev-odoo"
+echo "${VERDE}[+]${NC} Clonando odoo comunity en dentro de la carpeta dev-odoo"
 mkdir dev-odoo	
 cd dev-odoo		
 git clone https://github.com/odoo/odoo.git
 
-echo "[+] Instalando postgresql"
+echo "${VERDE}[+]${NC} Instalando postgresql"
 sudo apt-get install -y postgresql postgresql-contrib
 
-echo "[+] Creando usuario de postgreSQL"
+echo "${VERDE}[+]${NC} Creando usuario de postgreSQL"
 sudo su - postgres -c "createuser -s $USER"
 
-echo "[+] Instalando requerimientos de odoo"
+echo "${VERDE}[+]${NC} Instalando requerimientos de odoo"
 sudo apt-get install libxml2-dev libxslt-dev libevent-dev libsasl2-dev libldap2-dev
 sudo pip install -r requirements.txt 
 sudo apt-get install -y npm
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install -g less
 
-echo "[+] Ejecutando odoo"
+echo "${VERDE}[+]${NC} Ejecutando odoo"
 ./odoo.py --save --stop-after-init
 ./odoo.py
+
+# ---------------------------- Instalando ambiente para desarrollo ------
+echo "${VERDE}[+]${NC} Instalando herramientas de desarrollo"
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+sudo apt-get install terminator
+
